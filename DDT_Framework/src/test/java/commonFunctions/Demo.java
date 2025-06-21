372a186c0c2848f1ab23ef6f0074e45540bfe8e7
Qedge <Qedge@DESKTOP-1RLTEAV.Dlink>
2025-06-21 11:08:00 +0530

    New Class Created

---
+++ DDT_Framework/src/test/java/commonFunctions/Demo.java
@@ -0,0 +1,25 @@
+package commonFunctions;
+
+public class Demo {
+public void add()
+{
+	int a=87;
+	int b=8;
+	int c=a+b;
+	System.out.println(c);
+}
+public void div()
+{
+	int a=4564;
+	int b=4;
+	int c=a+b;
+	System.out.println(c);
+}
+public void mul()
+{
+	int a=765765;
+	int b=4;
+	int c=a+b;
+	System.out.println(c);
+}
+}
